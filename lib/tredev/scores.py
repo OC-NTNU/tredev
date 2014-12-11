24d06dea31ddbeee42236e5755045680189e77e5
Erwin Marsi <emarsi@idi.ntnu.no>
2014-12-11 19:30:15 +0100

    report no of predicted/gold positives/negatives

--- lib/tredev/scores.py
+++ lib/tredev/scores.py
@@ -8,8 +8,10 @@ from tredev.annots import Annotations
 class Scores(pd.DataFrame):
     
     stats = [ "precision", "recall", "f_score",
-              "positives", "negatives", "unknowns", "ignores", 
-              "true_pos", "false_pos", "true_neg", "false_neg" ]
+              "#pred_pos", "#pred_neg",
+              "#gold_pos", "#gold_neg", "#gold_unk", "#gold_ign", 
+              "#true_pos", "#false_pos", "#true_neg", "#false_neg",
+              "#unk_pos", "#unk_neg"]
     
     def __init__(self, *args, **kwargs):
         if kwargs.get("columns") is None:
@@ -34,10 +36,10 @@ class Scores(pd.DataFrame):
         is_unk = true_values == Annotations.unknown
         is_ign = true_values == Annotations.ignore
         
-        pos = sum(is_pos)
-        neg = sum(is_neg)
-        unk = sum(is_unk)
-        ign = sum(is_ign)
+        gold_pos = sum(is_pos)
+        gold_neg = sum(is_neg)
+        gold_unk = sum(is_unk)
+        gold_ign = sum(is_ign)
         
         pred_values = true_values.copy()
         pred_values[:] = Annotations.negative 
@@ -46,18 +48,28 @@ class Scores(pd.DataFrame):
             pred_values.loc[node_id] = Annotations.positive
             
         is_match = pred_values == Annotations.positive
-            
-        # unknowns and ignored are discounted
-        tp = sum(is_pos & is_match)
-        fp = sum(is_neg & is_match)
-        tn = sum(is_neg & ~is_match)
-        fn = sum(is_pos & ~is_match)
         
-        prec = (tp / (tp + fp)) * 100
-        rec = (tp / (tp + fn)) * 100
+        # predicting positives and predicted negatives (discounting nothing)
+        pred_pos = sum(is_match)
+        pred_neg = sum(~is_match)
+         
+        # true posities, false positives, true negatives and fasle negatives,  
+        # discounting unknown and ignored instances 
+        true_pos = sum(is_pos & is_match)
+        false_pos = sum(is_neg & is_match)
+        true_neg = sum(is_neg & ~is_match)
+        false_neg = sum(is_pos & ~is_match)
+        
+        prec = (true_pos / (true_pos + false_pos)) * 100
+        rec = (true_pos / (true_pos + false_neg)) * 100
         f = 2 * ((prec * rec) / (prec + rec))
         
-        scores =  prec, rec, f, pos, neg, unk, ign, tp, fp, tn, fn
+        # unknown positives and unknown negatives,
+        # discounting true, false and ignored instances
+        unk_pos = sum(is_unk & is_match)
+        unk_neg = sum(is_unk & ~is_match)
+        
+        scores =  prec, rec, f, pred_pos, pred_neg, gold_pos, gold_neg, gold_unk, gold_ign, true_pos, false_pos, true_neg, false_neg, unk_pos, unk_neg
         
         if name:
             # save score
